8ca056bbfa86957fe16af132092f27bfd1138f08
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2022-04-17 12:55:37 -0300

    refactor: move header and footer to components

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,4 +1,5 @@
-import Logo from '@/presentation/components/logo/logo'
+import Footer from '@/presentation/components/footer/footer'
+import LoginHeader from '@/presentation/components/login-header/login-header'
 import Spinner from '@/presentation/components/spinner/spinner'
 import React from 'react'
 import styles from './login-styles.scss'
@@ -6,10 +7,7 @@ import styles from './login-styles.scss'
 const Login: React.FC = () => {
   return (
     <div className={styles.login}>
-      <header className={styles.header}>
-        <Logo />
-        <h1>4Dev - Enquetes para programadores</h1>
-      </header>
+      <LoginHeader/>
       <form className={styles.form}>
         <h2>Login</h2>
         <div className={styles.inputWrap}>
@@ -33,7 +31,7 @@ const Login: React.FC = () => {
           <span className={styles.error}>Usuário ou senha inválidos</span>
         </div>
       </form>
-      <footer className={styles.footer} />
+      <Footer/>
     </div>
   )
 }
