b7c58d34a6496c6f77766cc8aff8d80e51abdc87
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2022-04-20 00:49:30 -0300

    feat: prevent Login will not call more than once

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -32,6 +32,9 @@ const Login: React.FC<LoginProps> = ({ validation, authentication }: LoginProps)
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
+    if (state.isLoading) {
+      return
+    }
     setState({
       ...state,
       isLoading: true
