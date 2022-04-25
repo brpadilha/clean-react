a284e5066c7a22cd9c6e4446ca3d74d064e65edb
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2022-04-25 18:05:04 -0300

    test: ensure ValidatiomBuilder has an email helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,9 +1,16 @@
-import { RequiredFieldValidation, ValidationBuilder } from '..'
+import { RequiredFieldValidation, ValidationBuilder as sut } from '..'
+import { EmailValidation } from '../email/email-validation'
 
 describe('ValidationBuilder', () => {
   test('should return RequiredFieldValidation ', () => {
-    const validations = ValidationBuilder.field('fieldName').required().build()
+    const validations = sut.field('fieldName').required().build()
 
     expect(validations).toEqual([new RequiredFieldValidation('fieldName')])
+  })
+
+  test('should return EmailValidation ', () => {
+    const validations = sut.field('any_field').email().build()
+
+    expect(validations).toEqual([new EmailValidation('any_field')])
   })
 })
