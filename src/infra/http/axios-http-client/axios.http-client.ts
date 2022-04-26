73e4eb4bf07f7a7a21e49b525449b77fc733060f
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2022-04-25 23:40:18 -0300

    feat: add Login composition

--- src/infra/http/axios-http-client/axios.http-client.ts
+++ src/infra/http/axios-http-client/axios.http-client.ts
@@ -1,6 +1,5 @@
-import { HttpPostParams, HttpResponse } from '@/data/protocols/http'
+import { HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/http'
 import axios from 'axios'
-import { HttpPostClient } from '@/data/protocols/http/http-post-client'
 
 export class AxiosHttpClient implements HttpPostClient<any, any> {
   async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
